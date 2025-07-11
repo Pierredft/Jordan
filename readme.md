d2b382cb49921f67e867b956d43736dbbec2ca4d
pierre defauquet <pierredefauquet@gmail.com>
2025-07-11 11:27:37 +0200

    first push

---
+++ readme.md
@@ -0,0 +1,19 @@
+# BONJOUR INTRÉPIDE CDA"
+Si tu lis ce readme c'est que tu as eu la curiosité de venir voir si sur mon repo je n'aurai pas un projet similaire à celui que je t'ai donner en exercice. Je te rassure ce n'est pas de la triche c'est juste de la veille technologique.
+
+# 80% de ce que j'ai demander ce trouve dans les documents que je t'ai fournis
+Mais tu trouvera un fichier 10-MailerGmail qui est une de tes futurs doc, elle permet de liéer ton application à une adresse de messagerie Gmail, et de pouvoir lors de ton register envoyer un mail de vérification à ton user.
+
+# Mais que ce passe t'il pour les 20 % restant ?
+À toi de trouver et de te balader, de vérifier les heures et les titre de mes derniers commit, peut être que tu bloque sur quelque chose et que tu trouvera la réponse ici, cela t'évitera d'allez demander à l'IA.
+
+# ATTENTION T'INSPIRER NE VEUX PAS DIRE TOUT COPIER
+
+
+
+# Bundle security register-form
+composer require symfony/security-bundle
+composer require symfonycasts/verify-email-bundle
+
+
+
---
+++ src/Form/RegistrationFormType.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Validator\Constraints as Assert;
+
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email',EmailType::class,[
+                'attr' =>[
+                    'class' => 'form-control border-jordan color-jordan-form',
+                    'minlength' => 2,
+                    'maxLenght' => 180,
+                    'autocomplete' => 'email',
+                    'placeholder' => 'user@gmail.com',
+                ],
+                    'label' => 'Adresse e-mail :',
+                    'label_attr' => [
+                        'class' => 'form-label',
+                    ],
+                    'constraints' => [
+                        new NotBlank([
+                            'message' => 'Merci de saisir votre adresse e-mail',
+                        ]),
+                        new Length([
+                            'max' => 180,
+                            'maxMessage' => 'Votre adresse e-mail peut contenir maximum {{ limit }} caractéres',
+                        ]),
+                        new Assert\Email([
+                            'message' => 'Merci de saisir une adresse e-mail valide',
+                        ])
+                    ],
+            ])
+            ->add('agreeTerms', CheckboxType::class, [
+                'attr' => [
+                    'class' => 'form-check-input ms-3',
+                ],
+                'label' => 'J\'accepte les mentions légales',
+                'mapped' => false,
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'Vous devez accepter les metions légales.',
+                    ]),
+                ],
+            ])
+            ->add('plainPassword', RepeatedType::class,[
+                'type' => PasswordType::class,
+                'first_options'=> [
+                    'attr' => [
+                        'class' => 'form-control border-jordan color-jordan-form',
+                        'placeholder' => 'Mot de passe',
+                    ],
+                    'label'=> 'Mot de passe :',
+                    'label_attr' => [
+                        'class' => 'form-label',
+                    ],
+                    'constraints' => [
+                        new Assert\NotBlank([
+                            'message' => 'Merci de saisir un mot de passe',
+                        ]),
+                        new Assert\Length([
+                            'min' => 6,
+                            'minMessage' => 'Votre mot de passe doit contenir au moins {{ limit }} caractéres',
+                            // max length allowed by Symfony for security reasons
+                            'max' => 4096,
+                        ]),
+                        new Assert\Regex([
+                            'pattern' => '/^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>_\-+=\/~`\[\];\'])[A-Za-z\d!@#$%^&*(),.?":{}|<>_\-+=\/~`\[\];\']{10,}$/',
+                            'message' => 'Votre mot de passe doit contenir au moins 10 caractères, une majuscule, un chiffre, un caractère spécial parmi (!@#$%^&*(),.?":{}|<>_-+=/~\[];\'), et ne doit pas contenir d’espace.'
+                        ]),
+                    ],
+                    'mapped' => false,
+                ],
+                'second_options' => [
+                    'attr' => [
+                        'class' => 'form-control border-jordan color-jordan-form',
+                        'placeholder' => 'Confirmer le mot de passe',
+                    ],
+                    'label' => 'Confirmer le mot de passe :',
+                    'label_attr' => [
+                        'class' => 'form-label',
+                    ],
+                ],
+                'constraints' => [
+                    new Assert\NotBlank([
+                        'message' => 'Merci de confirmer votre mot de passe',
+                    ]),
+                    new Assert\Length([
+                        'min' => 6,
+                        'minMessage' => 'Votre mot de passe doit contenir au moins {{ limit }} caractéres',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                    ]),
+                    new Assert\Regex([
+                        'pattern' => '/^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>_\-+=\/~`\[\];\'])[A-Za-z\d!@#$%^&*(),.?":{}|<>_\-+=\/~`\[\];\']{10,}$/',
+                        'message' => 'Votre mot de passe doit contenir au moins 10 caractères, une majuscule, un chiffre, un caractère spécial parmi (!@#$%^&*(),.?":{}|<>_-+=/~\[];\'), et ne doit pas contenir d’espace.'
+                    ]),
+                ],
+                'mapped' => false,
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
