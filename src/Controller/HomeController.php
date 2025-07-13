29e33c2a92aabb10626b38e587f507acfbe9bce7
pierre defauquet <pierredefauquet@gmail.com>
2025-07-13 13:13:10 +0200

    ajout d'une video pub sur le home, ajout d'un carrousel de card pour les promos

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,20 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(EntityManagerInterface $em): Response
     {
+        $product = $em->getRepository(Product::class)->findAll();
         return $this->render('home/home.html.twig', [
-            'controller_name' => 'HomeController',
+            'products' => $product,
         ]);
     }
 }
