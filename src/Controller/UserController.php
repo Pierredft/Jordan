8e8bc3bc544b2f75a0c8c4857280ad9444697b41
pierre defauquet <pierredefauquet@gmail.com>
2025-07-18 14:46:13 +0200

    modification des route pour user

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -23,7 +23,7 @@ final class UserController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'app_user_show', methods: ['GET'])]
+    #[Route('/{id}', name: 'app_user', methods: ['GET'])]
     public function show(User $user): Response
     {
         return $this->render('user/show.html.twig', [
